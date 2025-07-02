a6077a02b5337c80298e6b34b8b1c9e0d27d4a40
Paul Chege <pchegenjenga@gmail.com>
2025-07-02 21:28:16 +0300

    Updated ruby, rails and other gems

--- Gemfile
+++ Gemfile
@@ -8,17 +8,17 @@ git_source(:github) do |repo_name|
 end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.7'
+gem "rails", "~> 8.0.2"
 # Use postgres as the database for Active Record
 gem 'pg'
 # Use Puma as the app server
-gem 'puma', '~> 3.0'
+gem "puma", ">= 5.0"
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -27,36 +27,35 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'rack-cors'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
-  gem 'database_cleaner'
+  gem 'listen'
+
+  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem "debug", platforms: %i[ mswin mswin64 mingw x64_mingw jruby  ], require: "debug/prelude"
+
+  # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
+  gem "brakeman", require: false
+
+  # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
+  gem "rubocop-rails-omakase", require: false
+
+  gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'faker'
-  gem 'rspec-rails', '~> 3.5'
-  gem 'shoulda-matchers', '~> 3.1'
-  gem 'dotenv-rails'
+  gem 'shoulda-matchers'
+  gem 'database_cleaner-active_record'
 end
 
 group :development do
-  gem 'listen', '~> 3.0.5'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'annotate'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'annotaterb'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby  ]
 
 # API Authentication
 gem 'jwt'
-
 gem 'rb-readline'
-
-gem 'rubocop', require: false
-
 gem 'rack-cors'
-
-gem 'africastalking-ruby', '~> 2.1.6'
+gem 'africastalking-ruby'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -2,60 +2,58 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200722064815) do
-
+ActiveRecord::Schema[8.0].define(version: 2020_07_22_064815) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "lessons", force: :cascade do |t|
-    t.integer  "student_id"
-    t.time     "time"
-    t.integer  "duration"
-    t.text     "plan"
-    t.integer  "status"
-    t.integer  "charge"
-    t.boolean  "paid"
+    t.integer "student_id"
+    t.time "time"
+    t.integer "duration"
+    t.text "plan"
+    t.integer "status"
+    t.integer "charge"
+    t.boolean "paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date     "day"
-    t.index ["student_id"], name: "index_lessons_on_student_id", using: :btree
+    t.date "day"
+    t.index ["student_id"], name: "index_lessons_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "instrument"
-    t.date     "start_date"
-    t.string   "institution"
-    t.string   "mobile_number"
-    t.date     "date_of_birth"
-    t.integer  "lesson_day"
-    t.time     "lesson_time"
-    t.text     "goals"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "lesson_duration"
-    t.integer  "lesson_charge"
-    t.index ["user_id"], name: "index_students_on_user_id", using: :btree
+    t.string "name"
+    t.string "email"
+    t.string "instrument"
+    t.date "start_date"
+    t.string "institution"
+    t.string "mobile_number"
+    t.date "date_of_birth"
+    t.integer "lesson_day"
+    t.time "lesson_time"
+    t.text "goals"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "lesson_duration"
+    t.integer "lesson_charge"
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "google_id"
-    t.string   "google_photo_url"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "google_id"
+    t.string "google_photo_url"
   end
-
 end
